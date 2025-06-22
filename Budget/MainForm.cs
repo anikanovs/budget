837b8a78473d93c98a52b6a31473ab778a151695
Sergey Anikanov <anikanovs@yandex.ru>
2025-06-22 19:21:31 +0300

    Main form

---
+++ Budget/MainForm.cs
@@ -0,0 +1,22 @@
+namespace Budget
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // MainForm
+            // 
+            ClientSize = new Size(284, 261);
+            Name = "MainForm";
+            ResumeLayout(false);
+
+        }
+    }
+}
